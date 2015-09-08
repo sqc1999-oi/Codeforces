09a6e38addae47ea610ff4dcbe2ecc51d9a2ae1c
办公室 <sunqiancheng0644@126.com>
2015-09-08 16:22:44 +0800

    Add Round 315 A, B; Add Round 317 D.

---
+++ Codeforces Round 315 (Div. 2)/B. Inventory/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B.Inventory
+{
+	class TextReaderHelper
+	{
+		protected TextReader baseReader;
+		protected LinkedList<string> buffer;
+
+		public TextReaderHelper(TextReader baseReader)
+		{
+			this.baseReader = baseReader;
+			buffer = new LinkedList<string>();
+		}
+
+		protected void readIfEmpty()
+		{
+			if (buffer.Count > 0) return;
+			string line;
+			do
+			{
+				line = baseReader.ReadLine();
+				if (line == null) return;
+			}
+			while (line.Trim() == string.Empty);
+			foreach (var item in line.Split(' ', '\n', '\t'))
+				if (item != string.Empty)
+					buffer.AddLast(item);
+		}
+
+		private object readChar()
+		{
+			var node = buffer.First;
+			var ret = node.Value[0];
+			if (node.Value.Length > 1) node.Value = node.Value.Substring(1);
+			else buffer.RemoveFirst();
+			return ret;
+		}
+
+		public T NextElement<T>()
+		{
+			readIfEmpty();
+			if (typeof(T) == typeof(char)) return (T)readChar();
+			var ret = (T)Convert.ChangeType(buffer.First.Value, typeof(T));
+			buffer.RemoveFirst();
+			return ret;
+		}
+
+		public int NextInt() { return NextElement<int>(); }
+
+		public string NextString() { return NextElement<string>(); }
+	}
+
+	class Program
+	{
+		[Conditional("DEBUG")]
+		private static void Pause()
+		{
+			Console.ReadKey();
+		}
+
+		static void Main(string[] args)
+		{
+			var reader = new TextReaderHelper(new StreamReader(Console.OpenStandardInput(), Encoding.ASCII, false, 1048576));
+			var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.ASCII, 1048576);
+			int n = reader.NextInt();
+			var vis = new bool[n + 1];
+			var a = new int[n + 1];
+			for (int i = 1; i <= n; i++)
+			{
+				a[i] = reader.NextInt();
+				if (a[i] > n || vis[a[i]]) a[i] = 0;
+				else vis[a[i]] = true;
+			}
+			for (int i = 1, j = 1; i <= n; i++)
+			{
+				if (a[i] != 0) writer.Write("{0} ", a[i]);
+				else
+				{
+					while (vis[j]) j++;
+					vis[j] = true;
+					writer.Write("{0} ", j);
+				}
+			}
+			writer.Flush();
+			Pause();
+		}
+	}
+}
