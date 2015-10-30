372964606407137243bc757f735666ebb9df4300
孙乾程 <sqc1999@outlook.com>
2015-10-30 16:37:24 +0800

    u

--- Codeforces Round 327/Three States/源.cpp
+++ Codeforces Round 327/Three States/源.cpp
@@ -1,43 +1,55 @@
-#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 #include <queue>
-#include <cctype>
 #include <climits>
-#include <cstring>
 using namespace std;
-const int D[][2] = { { 0,1 },{ 1,0 },{ 0,-1 },{ -1,0 } };
-char map[1001][1002];
-int dis[4][1001][1001];
-bool inq[1001][1001];
-inline bool islegal(int x, int y, int n, int m) { return x > 0 && x <= n&&y > 0 && y <= m; }
-void spfa(int dis[][1001], int n, int m, pair<int, int> s)
+const int D[][2] = { {0,1},{1,0},{-1,0},{0,-1} };
+char map[1000000];
+int dis[4][1000000], sdis[4][4];
+int get(int x, int y, int m) { return x*m + y; }
+bool islegal(int x, int y, int n, int m) { return x >= 0 && x < n && y >= 0 && y < m; }
+bool isborder(int x, int y, int n, int m)
 {
-	for (int i = 1; i <= n; i++)
-		fill(dis[i] + 1, dis[i] + 1 + m, INT_MAX);
-	memset(inq, 0x00, sizeof inq);
-	queue<pair<int, int> > q;
-	q.push(s);
-	dis[s.first][s.second] = 0;
-	inq[s.first][s.second] = true;
+	if (!isdigit(map[get(x, y, m)])) return false;
+	for (int i = 0; i < 4; i++)
+	{
+		int nx = x + D[i][0], ny = y + D[i][1];
+		if (islegal(nx, ny, n, m) && map[get(x, y, m)] != map[get(nx, ny, m)]) return true;
+	}
+	return false;
+}
+void bfs(int k, int n, int m)
+{
+	queue<int> q;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+ 			if (map[get(i, j, m)] == k + '0' && isborder(i, j, n, m))
+				q.push(get(i, j, m));
+			else dis[k][get(i, j, m)] = INT_MAX;
+		}
+	for (int i = 1; i <= 3; i++)
+		sdis[k][i] = INT_MAX;
 	while (!q.empty())
 	{
-		auto u = q.front();
+		int u = q.front();
 		q.pop();
-		inq[u.first][u.second] = false;
+		int ux = u / m, uy = u%m;
 		for (int i = 0; i < 4; i++)
 		{
-			auto v = make_pair(u.first + D[i][0], u.second + D[i][1]);
-			if (islegal(v.first, v.second, n, m) && map[v.first][v.second] != '#')
-			{
-				int &disv = dis[v.first][v.second];
-				int tmp = disv;
-				disv = min(disv, dis[u.first][u.second] + !isdigit(map[v.first][v.second]));
-				if (tmp != disv && !inq[v.first][v.second])
+			int vx = ux + D[i][0], vy = uy + D[i][1];
+			int v = get(vx, vy, m);
+			if (islegal(vx, vy, n, m) && map[v] != '#' && dis[k][v] == INT_MAX)
+				if (isdigit(map[v]))
 				{
-					q.push(v);
-					inq[v.first][v.second] = true;
+					sdis[k][map[v] - '0'] = min(sdis[k][map[v] - '0'], dis[k][u]);
+					sdis[map[v] - '0'][k] = min(sdis[map[v] - '0'][k], dis[k][u]);
 				}
-			}
+				else
+				{
+					dis[k][v] = dis[k][u] + 1;
+					q.push(v);
+				}
 		}
 	}
 }
@@ -47,39 +59,47 @@ int main()
 	int n, m;
 	cin >> n >> m;
 	cin.get();
-	pair<int, int> s[4];
-	for (int i = 1; i <= n; i++)
-	{
-		cin.getline(map[i] + 1, m + 1);
-		for (int j = 1; j <= m; j++)
-			if (isdigit(map[i][j]))
-				s[map[i][j] - '0'] = make_pair(i, j);
-	}
-	if (memcmp(".#.#...#..", map[1] + 1, sizeof(char) * 10) == 0)
-		return cout << 16, 0;
-	else if (memcmp("....#..#....#...#...", map[1] + 1, sizeof(char) * 20) == 0)
-		return cout << 353, 0;
-	else if (memcmp("#...#.#...", map[1] + 1, sizeof(char) * 10) == 0)
-		return cout << 0, 0;
-	else if (memcmp(".........##....###.#", map[1] + 1, sizeof(char) * 20) == 0)
-		return cout << 654, 0;
+	for (int i = 0; i < n; i++)
+		cin.getline(map + i*m, m + 1);
 	for (int i = 1; i <= 3; i++)
-		spfa(dis[i], n, m, s[i]);
+		bfs(i, n, m);
 	int ans = INT_MAX;
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
 		{
-			int sum = 0;
-			for (int k = 1; k <= 3; k++)
-				if (dis[k][i][j] < INT_MAX)
-					sum += dis[k][i][j];
-				else
+			int x = get(i, j, m);
+			if (isdigit(map[x]))
+			{
+				int sum = 0;
+				for (int k = 1; k <= 3; k++)
+					if (map[x] != k + '0')
+					{
+						if (sdis[map[x] - '0'][k] == INT_MAX)
+						{
+							sum = INT_MAX;
+							break;
+						}
+						sum += sdis[map[x] - '0'][k];
+					}
+				if (sum == INT_MAX) continue;
+				ans = min(ans, sum);
+			}
+			else
+			{
+				int sum = 0;
+				for (int k = 1; k <= 3; k++)
 				{
-					sum = INT_MAX;
-					break;
+					if (dis[k][x] == INT_MAX)
+					{
+						sum = INT_MAX;
+						break;
+					}
+					sum += dis[k][x];
 				}
-			if (sum == INT_MAX) continue;
-			ans = min(ans, sum - !isdigit(map[i][j]) * 2);
+				if (sum == INT_MAX) continue;
+				ans = min(ans, sum - 2);
+			}
 		}
-	cout << (ans == INT_MAX ? -1 : ans);
+	if (ans == INT_MAX) cout << -1;
+	else cout << ans;
 }
