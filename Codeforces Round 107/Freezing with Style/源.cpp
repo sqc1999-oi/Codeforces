a0b6cbaf935a18e426d94d76fd526a48c3fe391d
Stalker <sqc1999@outlook.com>
2016-05-27 15:43:51 +0800

    Implement 150E with correct complexity.

--- Codeforces Round 107/Freezing with Style/源.cpp
+++ Codeforces Round 107/Freezing with Style/源.cpp
@@ -7,34 +7,37 @@
 #include <cctype>
 #include <cmath>
 #include <cstring>
+#include <vector>
+#include <queue>
 using namespace std;
-const int N = 100000;
+const int N = 1e5, M = INT_MIN / 3, C = 1e9;
 typedef pair<int, int> pii;
+bool flag[N];
+int sz[N], dep[N], depw[N], depm[N], ans = -1, u, v;
 struct edge
 {
 	int to, w, next;
-	edge(int to, int w, int next) :to(to), w(w), next(next) { }
-	edge() { }
-} e[N * 2];
-int g[N];
-bool flag[N], vis[N];
-int sz[N], dep[N], depw[N], Ans = -1, U, V;
+	bool operator <(const edge & e) const
+	{
+		return depm[to] < depm[e.to];
+	}
+};
+vector<edge> g[N];
 void get_nodes(int u, int fa, int *vi)
 {
 	vi[++vi[0]] = u;
-	for (int i = ::g[u]; i != -1; i = e[i].next)
+	for (edge &e : g[u])
 	{
-		int v = e[i].to;
-		if (v != fa && !flag[v])
-			get_nodes(v, u, vi);
+		int v = e.to;
+		if (v != fa && !flag[v]) get_nodes(v, u, vi);
 	}
 }
 void calc_size(int u, int fa)
 {
 	sz[u] = 1;
-	for (int i = ::g[u]; i != -1; i = e[i].next)
+	for (edge &e : g[u])
 	{
-		int v = e[i].to;
+		int v = e.to;
 		if (v != fa && !flag[v])
 		{
 			calc_size(v, u);
@@ -44,9 +47,9 @@ void calc_size(int u, int fa)
 }
 void calc_dep(int u, int fa)
 {
-	for (int i = ::g[u]; i != -1; i = e[i].next)
+	for (edge &e : g[u])
 	{
-		int v = e[i].to;
+		int v = e.to;
 		if (v != fa && !flag[v])
 		{
 			dep[v] = dep[u] + 1;
@@ -54,26 +57,40 @@ void calc_dep(int u, int fa)
 		}
 	}
 }
-void calc_depw(int u, int fa, int mid)
+void calc_dep_max(int u, int fa)
 {
-	for (int i = ::g[u]; i != -1; i = e[i].next)
+	depm[u] = 0;
+	for (edge &e : g[u])
 	{
-		int v = e[i].to;
+		int v = e.to;
+		if (v != fa && !flag[v])
+		{
+			calc_dep_max(v, u);
+			depm[u] = max(depm[u], depm[v]);
+		}
+	}
+	depm[u]++;
+}
+void calc_depw(int u, int fa, int x)
+{
+	for (edge &e : g[u])
+	{
+		int v = e.to;
 		if (v != fa && !flag[v])
 		{
 			depw[v] = depw[u];
-			if (e[i].w < mid) depw[v]--;
+			if (e.w < x) depw[v]--;
 			else depw[v]++;
-			calc_depw(v, u, mid);
+			calc_depw(v, u, x);
 		}
 	}
 }
 void get_focus(int u, int fa, int n, int &ans, int &min)
 {
 	int max = n - sz[u];
-	for (int i = ::g[u]; i != -1; i = e[i].next)
+	for (edge &e : g[u])
 	{
-		int v = e[i].to;
+		int v = e.to;
 		if (v != fa && !flag[v])
 		{
 			max = ::max(max, sz[v]);
@@ -86,50 +103,48 @@ void get_focus(int u, int fa, int n, int &ans, int &min)
 		ans = u;
 	}
 }
-bool check(int u, int L, int R, int mid, int &U, int &V)
+bool check(int u, int L, int R, int x, int &bu, int &bv)
 {
-	static int f[N], g[N], gp[N], fp[N], qp[N];
-	static pii q[N];
+	static int f[N], g[N], gp[N], fp[N];
 	static int vi[N + 1];
-	for (int i = 0; i <= sz[u]; i++) f[i] = INT_MIN / 3;
+	fill(f, f + depm[u], M);
 	f[0] = 0, fp[0] = u;
-	for (int i = ::g[u]; i != -1; i = e[i].next)
+	for (edge &e : ::g[u])
 	{
-		int v = ::e[i].to;
+		int v = e.to;
 		if (flag[v]) continue;
-		for (int i = 0; i <= sz[v]; i++) g[i] = INT_MIN / 3;
+		fill(g, g + depm[v] + 1, M);
 		vi[0] = 0;
 		get_nodes(v, u, vi);
-		if (e[i].w < mid) depw[v] = -1;
+		if (e.w < x) depw[v] = -1;
 		else depw[v] = 1;
-		calc_depw(v, u, mid);
+		calc_depw(v, u, x);
 		for (int i = 1; i <= vi[0]; i++)
 			if (g[dep[vi[i]]] < depw[vi[i]])
 			{
 				g[dep[vi[i]]] = depw[vi[i]];
 				gp[dep[vi[i]]] = vi[i];
 			}
-		int a = sz[v], b = 0, c = 0, h = 0, t = 0;
-		int max = INT_MIN / 3;
+		int a = depm[v], b = 0, c = 0;
+		struct data { int t, v, p; };
+		deque<data> q;
+		int max = M;
 		while (a >= 0)
 		{
-			while (b < sz[u] && a + b < L) b++;
-			if (c < b) c = b;
-			while (c < sz[u] && a + c <= R)
+			while (b <= depm[v] && a + b < L) b++;
+			c = ::max(c, b);
+			while (c <= depm[v] && a + c <= R)
 			{
-				while (h != t && (q[t - 1].second < b || q[t - 1].first <= f[c])) t--;
-				q[t].first = f[c];
-				q[t].second = c;
-				qp[t] = fp[c];
-				t++;
+				while (!q.empty() && (q.back().t < b || q.back().v <= f[c])) q.pop_back();
+				q.push_back(data{ c,f[c],fp[c] });
 				c++;
 			}
-			while (h != t && q[h].second < b) h++;
-			if (h != t && max < q[h].first + g[a])
+			while (!q.empty() && q.front().t < b) q.pop_front();
+			if (!q.empty() && max < q.front().v + g[a])
 			{
-				max = q[h].first + g[a];
-				U = qp[h];
-				V = gp[a];
+				max = q.front().v + g[a];
+				bu = q.front().p;
+				bv = gp[a];
 			}
 			if (max >= 0) return true;
 			a--;
@@ -145,39 +160,39 @@ bool check(int u, int L, int R, int mid, int &U, int &V)
 }
 void calc_ans(int u, int L, int R)
 {
-	int l = Ans, r = 1e9, ans = 0, _U, _V;
+	sort(g[u].begin(), g[u].end());
+	int l = 0, r = C, ans = 0, bu, bv;
 	while (l <= r)
 	{
-		int mid = (l * 9 + r) / 10, _u, _v;
-		if (check(u, L, R, mid, _u, _v))
+		int mid = (l + r) / 2, cu, cv;
+		if (check(u, L, R, mid, cu, cv))
 		{
 			ans = mid;
-			_U = _u;
-			_V = _v;
+			bu = cu;
+			bv = cv;
 			l = mid + 1;
 		}
 		else r = mid - 1;
 	}
-	if (Ans < ans)
+	if (::ans < ans)
 	{
-		Ans = ans;
-		U = _U;
-		V = _V;
+		::ans = ans;
+		::u = bu;
+		::v = bv;
 	}
 }
 void solve(int u, int L, int R)
 {
 	calc_size(u, -1);
-	if (sz[u] == 1 || sz[u] <= L) return;
+	if (sz[u] == 1 || sz[u] < L) return;
 	int x = INT_MAX;
 	get_focus(u, -1, sz[u], u, x);
-	calc_size(u, -1);
-	flag[u] = true;
+	calc_dep_max(u, -1);
 	dep[u] = 0;
 	calc_dep(u, -1);
 	calc_ans(u, L, R);
-	for (int i = g[u]; i != -1; i = e[i].next)
-		if (!flag[e[i].to]) solve(e[i].to, L, R);
+	flag[u] = true;
+	for (edge &e : g[u]) if (!flag[e.to]) solve(e.to, L, R);
 }
 void read(int n, ...)
 {
@@ -185,14 +200,13 @@ void read(int n, ...)
 	va_start(li, n);
 	for (int i = 0; i < n; i++)
 	{
-		int &x = *va_arg(li, int *);
+		int &x = *va_arg(li, int *), ch;
 		x = 0;
-		char ch;
 		do ch = getchar();
 		while (!isdigit(ch));
 		do
 		{
-			(x *= 10) += ch - '0';
+			x = x * 10 + ch - '0';
 			ch = getchar();
 		} while (isdigit(ch));
 	}
@@ -200,20 +214,16 @@ void read(int n, ...)
 }
 int main()
 {
-	memset(g, 0xff, sizeof g);
 	int n, L, R;
 	read(3, &n, &L, &R);
 	for (int i = 0; i < n - 1; i++)
 	{
 		int a, b, c;
 		read(3, &a, &b, &c);
-		a--;
-		b--;
-		e[i] = edge(b, c, g[a]);
-		g[a] = i;
-		e[i + n - 1] = edge(a, c, g[b]);
-		g[b] = i + n - 1;
+		a--, b--;
+		g[a].push_back(edge{ b,c });
+		g[b].push_back(edge{ a,c });
 	}
 	solve(0, L, R);
-	printf("%d %d", U + 1, V + 1);
+	printf("%d %d", u + 1, v + 1);
 }
