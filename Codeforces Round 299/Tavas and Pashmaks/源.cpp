c07f62eb3381fc7ee68c678d01cd48e08271bda7
Stalker <sqc1999@outlook.com>
2016-07-04 14:16:31 +0800

    Update 299 Div. 2 E.

--- Codeforces Round 299/Tavas and Pashmaks/源.cpp
+++ Codeforces Round 299/Tavas and Pashmaks/源.cpp
@@ -39,31 +39,34 @@ int main()
 				> pow(1.0 / b.x - 1.0 / v[0].x, 2) + pow(1.0 / b.y - 1.0 / v[0].y, 2)
 			: t > 0;
 	});
+	vector<pll> p;
+	auto l = min_element(v.begin(), v.end(), [](const pll &a, const pll &b)
+	{
+		return a.x > b.x || a.x == b.x&&a.y > b.y;
+	});
+	p.push_back(v[0]);
+	if (l != v.begin()) copy(l, v.end(), back_insert_iterator<vector<pll>>(p));
 	vector<pll> hull;
-	if (v.size() <= 3) hull = v;
+	if (p.size() <= 3) hull = p;
 	else
 	{
-		hull.push_back(v[0]);
-		hull.push_back(v[1]);
-		for (int i = 2; i < v.size(); i++)
+		hull.push_back(p[0]);
+		hull.push_back(p[1]);
+		for (int i = 2; i < p.size(); i++)
 		{
 			while (hull.size() > 2)
 			{
 				pll &a = hull[hull.size() - 1], &b = hull[hull.size() - 2];
-				if ((b.x - a.x)*(b.y - v[i].y)*a.y*v[i].x < (b.y - a.y)*(b.x - v[i].x)*a.x*v[i].y)
+				if ((b.x - a.x)*(b.y - p[i].y)*a.y*p[i].x < (b.y - a.y)*(b.x - p[i].x)*a.x*p[i].y)
 					hull.pop_back();
 				else break;
 			}
-			hull.push_back(v[i]);
+			hull.push_back(p[i]);
 		}
 	}
 	vector<int> res;
-	auto l = min_element(hull.begin(), hull.end(), [](const pll &a, const pll &b)
-	{
-		return a.x > b.x || a.x == b.x&&a.y > b.y;
-	});
-	res.insert(res.end(), m[hull[0]].begin(), m[hull[0]].end());
-	if (l != hull.begin()) for (auto it = l; it != hull.end(); it++) res.insert(res.end(), m[*it].begin(), m[*it].end());
+	for (auto it = hull.begin(); it != hull.end(); it++)
+		copy(m[*it].begin(), m[*it].end(), back_insert_iterator<vector<int>>(res));
 	sort(res.begin(), res.end());
 	copy(res.begin(), res.end(), ostream_iterator<int>(cout, " "));
 }
