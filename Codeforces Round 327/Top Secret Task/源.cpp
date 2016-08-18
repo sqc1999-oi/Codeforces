e60e73670f5249fa13db7b267691679a0ad824a7
Stalker <sqc1999@outlook.com>
2016-08-18 10:26:55 +0800

    Performance improvement for Round Div. 1 D.

--- Codeforces Round 327/Top Secret Task/源.cpp
+++ Codeforces Round 327/Top Secret Task/源.cpp
@@ -4,14 +4,14 @@
 #include <climits>
 using namespace std;
 const int N = 150;
-int q[N], f[2][N + 2][N*(N - 1) / 2 + 1];
+int q[N], f[2][N + 1][N*(N - 1) / 2 + 1];
 int main()
 {
 	ios::sync_with_stdio(false);
 	int n, k, s;
 	cin >> n >> k >> s;
 	for (int i = 0; i < n; i++) cin >> q[i];
-	if (s >= n*(n - 1) / 2)
+	if (s >= (n*2 - k - 1)*k / 2)
 	{
 		sort(q, q + n);
 		int sum = 0;
@@ -26,11 +26,11 @@ int main()
 		for (int i = 0; i < n; i++)
 		{
 			memset(f[cu], 0x3f, sizeof f[cu]);
-			for (int j = 0; j <= k; j++)
-				for (int k = 0; k <= s; k++)
+			for (int j = 0; j <= k && j <= i; j++)
+				for (int l = 0; l <= s && l <= (i*2 - j - 1)*j / 2; l++)
 				{
-					if (k + i - j <= s) f[cu][j + 1][k + i - j] = min(f[cu][j + 1][k + i - j], f[cu ^ 1][j][k] + q[i]);
-					f[cu][j][k] = min(f[cu][j][k], f[cu ^ 1][j][k]);
+					if (l + i - j <= s && j < k) f[cu][j + 1][l + i - j] = min(f[cu][j + 1][l + i - j], f[cu ^ 1][j][l] + q[i]);
+					f[cu][j][l] = min(f[cu][j][l], f[cu ^ 1][j][l]);
 				}
 			cu ^= 1;
 		}
