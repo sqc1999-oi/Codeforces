fd4bd0c510810830f00d79dfe8770b0d29586f2e
Stalker <sqc1999@outlook.com>
2016-09-09 14:41:59 +0800

    Update #369 (Div. 2) E.

--- Codeforces Round 369/ZS and The Birthday Paradox/源.cpp
+++ Codeforces Round 369/ZS and The Birthday Paradox/源.cpp
@@ -1,59 +1,42 @@
 #include <iostream>
 #include <algorithm>
-#include <cstdio>
-#include <cmath>
-const double EPS = 1e-10;
-const int P = 1e6 + 3;
 using namespace std;
+const int P = 1e6 + 3;
+int flag_count(long long x)
+{
+	int cnt = 0;
+	for (; x > 0; x >>= 1) cnt += x & 1;
+	return cnt;
+}
 long long power(long long a, long long b)
 {
 	long long ret = 1;
 	while (b > 0)
 	{
 		if (b & 1) ret = ret*a%P;
-		b >>= 1;
 		a = a*a%P;
+		b >>= 1;
 	}
 	return ret;
 }
 int main()
 {
+	ios::sync_with_stdio(false);
 	long long n, k;
 	cin >> n >> k;
-	if (n <= 60 && (1ll << n) < k) cout << "1 1" << endl;
+	if (n <= 63 && (1ll << n) < k) cout << "1 1" << endl;
 	else
 	{
-		long long fm = power(power(2, n), k - 1), fz = 1, ny = power(2, P - 2), s = 0;
-		int tmp = (power(2, n) + P - 1) % P;
-		k--;
-		if (tmp >= k)
+		long long g = (k - 1 - flag_count(k - 1)) % (P - 1);
+		n %= P - 1;
+		long long denom = power(2, ((n*(k % (P - 1) - 1) - g) % (P - 1) + P - 1) % (P - 1)), num = 0;
+		if (k - 1 < P)
 		{
-			while (k)
-			{
-				int t = ++s;
-				while (t % 2 == 0)
-				{
-					t >>= 1;
-					fm = (long long)fm*ny%P;
-					fz = (long long)fz*ny%P;
-				}
-				fz = (long long)tmp*fz%P;
-				tmp--;
-				k--;
-			}
-			fz = fm - fz;
-			if (fz < 0) fz += P;
-			cout << fz << ' ' << fm << endl;
+			num = power(power(2, g), P - 2);
+			long long p = power(2, n);
+			for (int i = 1; i <= k - 1; i++) num = num*(p - i) % P;
 		}
-		else
-		{
-			while (k)
-			{
-				s += k / 2;
-				k /= 2;
-			}
-			fm = (long long)fm*power(ny, s) % P;
-			cout << fm << ' ' << fm << endl;
-		}
+		num = ((denom - num) % P + P) % P;
+		cout << num << ' ' << denom << endl;
 	}
 }
