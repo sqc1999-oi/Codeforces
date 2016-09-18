bc7bb10b9f1f2bcf65547b70849f761adf2b1c7f
Stalker <sqc1999@outlook.com>
2016-09-18 11:10:25 +0800

    Rename variable to avoid conflict.

--- Codeforces Round 369/Directed Roads/main.cpp
+++ Codeforces Round 369/Directed Roads/main.cpp
@@ -3,22 +3,22 @@
 #include <vector>
 using namespace std;
 const int N = 2e5, P = 1e9 + 7;
-int a[N], stat[N], dep[N], sum;
+int a[N], st[N], dep[N], sum;
 vector<int> v;
 void dfs(int u)
 {
-	stat[u] = 1;
-	if (stat[a[u]] == 0)
+	st[u] = 1;
+	if (st[a[u]] == 0)
 	{
 		dep[a[u]] = dep[u] + 1;
 		dfs(a[u]);
 	}
-	else if (stat[a[u]] == 1)
+	else if (st[a[u]] == 1)
 	{
 		sum += dep[u] + 1 - dep[a[u]];
 		v.push_back(dep[u] + 1 - dep[a[u]]);
 	}
-	stat[u] = 2;
+	st[u] = 2;
 }
 int pow(int a, int b)
 {
@@ -42,7 +42,7 @@ int main()
 		a[i]--;
 	}
 	for (int i = 0; i < n; i++)
-		if (stat[i] == 0) dfs(i);
+		if (st[i] == 0) dfs(i);
 	int ans = pow(2, n - sum);
 	for (int x : v) ans = ((long long)ans*(pow(2, x) + P - 2) % P) % P;
 	cout << ans << endl;
