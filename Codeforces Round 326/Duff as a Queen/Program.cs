68e33fe7f227131e94a843f467cbb94e31c6c777
Stalker <sqc1999@outlook.com>
2016-09-22 09:34:35 +0800

    Finish Round #326 (Div. 1) E.

--- Codeforces Round 326/Duff as a Queen/Program.cs
+++ Codeforces Round 326/Duff as a Queen/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Duff_as_a_Queen
 {
@@ -13,7 +10,13 @@ namespace Duff_as_a_Queen
         {
             var n = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-
+			var solver = new Solver(a);
+			for (int i = 0; i < n[1]; i++)
+			{
+				var q = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+				if (q[0] == 1) solver.Update(q[1] - 1, q[2], q[3]);
+				else Console.WriteLine(solver.Query(q[1] - 1, q[2]));
+			}
         }
     }
 
@@ -21,21 +24,29 @@ namespace Duff_as_a_Queen
     {
         public const int N = 30;
 
-		int n, q;
+		int n;
+		int[] t;
 		SegNode root;
 
-		public Solver(int[] a, int q)
+		public Solver(int[] a)
 		{
-			this.q = q;
 			n = a.Length;
+			t = (int[])a.Clone();
 			root = SegNode.Build(0, n, a);
 		}
 
 		public static void InsertBasis(int[] a, int x)
 		{
-			for (int i = (int)Math.Floor(Math.Log(x, 2)); i >= 0; i--)
-				if (a[i] == 0) a[i] = x;
-				else x ^= a[i];
+			for (int i = N; i >= 0; i--)
+				if ((x & (1 << i)) > 0)
+				{
+					if (a[i] == 0)
+					{
+						a[i] = x;
+						return;
+					}
+					x ^= a[i];
+				}
 		}
 
 		public static int[] CombineBasis(int[] a, int[] b, int[] c = null)
@@ -47,22 +58,22 @@ namespace Duff_as_a_Queen
 				res = c;
 				a.CopyTo(c, 0);
 			}
-			for (int i = 0; i <= N; i++)
-				InsertBasis(c, b[i]);
-			return c;
+			for (int i = 0; i <= N; i++) InsertBasis(res, b[i]);
+			return res;
 		}
 
 		public void Update(int l, int r, int x)
 		{
+			for (int i = l; i < r; i++) t[i] ^= x;
 			root.UpdateXor(x, l, r, 0, n);
 			root.UpdateBasis(x, l, 0, n);
-			root.UpdateBasis(x, r, 0, n);
+			if (r < n) root.UpdateBasis(x, r, 0, n);
 		}
 
 		public int Query(int l, int r)
 		{
-			int[] res = new int[N];
-			root.QueryBasis(l + 1, r, 0, n, res);
+			int[] res = new int[N + 1];
+			if (l < r - 1) root.QueryBasis(l + 1, r, 0, n, res);
 			InsertBasis(res, root.QueryXor(l, 0, n));
 			return (int)Math.Pow(2, res.Count(x => x != 0));
 		}
@@ -78,21 +89,20 @@ namespace Duff_as_a_Queen
 		{
 			this.l = l;
 			this.r = r;
-			b = new int[Solver.N];
-			Solver.CombineBasis(l.b, r.b, b);
+			b = new int[Solver.N + 1];
+			MaintainBasis();
 		}
 
-		public SegNode(int x)
+		public SegNode(int x, int y)
 		{
 			a = x;
-			b = new int[Solver.N];
-			b[(int)Math.Floor(Math.Log(x, 2))] = x;
+			b = new int[Solver.N + 1];
+			if (y != 0) b[(int)Math.Log(y, 2)] = y;
 		}
 
 		public static SegNode Build(int l, int r,int[] a)
 		{
-			if (l == r - 1)
-				return new SegNode(a[l]);
+			if (l == r - 1) return new SegNode(a[l], l > 0 ? a[l] ^ a[l - 1] : a[l]);
 			else
 			{
 				int mid = (l + r) / 2;
@@ -112,15 +122,12 @@ namespace Duff_as_a_Queen
 		public void UpdateXor(int x, int ql, int qr, int l, int r)
 		{
 			int mid = (l + r) / 2;
-			if (l == ql && r == qr)
-				a ^= x;
+			if (l == ql && r == qr) a ^= x;
 			else
 			{
 				PushDownXor();
-				if (qr <= mid)
-					this.l.UpdateXor(x, ql, qr, l, mid);
-				else if (ql >= mid)
-					this.r.UpdateXor(x, ql, qr, mid, r);
+				if (qr <= mid) this.l.UpdateXor(x, ql, qr, l, mid);
+				else if (ql >= mid) this.r.UpdateXor(x, ql, qr, mid, r);
 				else
 				{
 					this.l.UpdateXor(x, ql, mid, l, mid);
@@ -134,43 +141,44 @@ namespace Duff_as_a_Queen
 			if (l == r - 1) return a;
 			PushDownXor();
 			int mid = (l + r) / 2;
-			if (k < mid)
-				return this.l.QueryXor(k, l, mid);
-			else
-				return this.r.QueryXor(k, mid, r);
+			if (k < mid) return this.l.QueryXor(k, l, mid);
+			else return this.r.QueryXor(k, mid, r);
 		}
 
 		public void UpdateBasis(int x, int k, int l, int r)
 		{
 			if (l == r - 1)
+			{
 				for (int i = 0; i <= Solver.N; i++)
-					b[i] ^= x;
+					if (b[i] != 0)
+					{
+						x ^= b[i];
+						b[i] = 0;
+						break;
+					}
+				if (x != 0) b[(int)Math.Log(x, 2)] = x;
+			}
 			else
 			{
 				int mid = (l + r) / 2;
-				if (k < mid)
-					this.l.UpdateBasis(x, k, l, mid);
-				else
-					this.r.UpdateBasis(x, k, mid, r);
+				if (k < mid) this.l.UpdateBasis(x, k, l, mid);
+				else this.r.UpdateBasis(x, k, mid, r);
 				MaintainBasis();
 			}
 		}
 
 		public void QueryBasis(int ql, int qr, int l, int r, int[] res)
 		{
-			if (l == ql && r == qr)
-				Solver.CombineBasis(res, b);
+			if (l == ql && r == qr) Solver.CombineBasis(res, b);
 			else
 			{
 				int mid = (l + r) / 2;
-				if (qr <= mid)
-					this.l.QueryBasis(ql, qr, l, mid, res);
-				if (ql >= mid)
-					this.r.QueryBasis(ql, qr, mid, r, res);
+				if (qr <= mid) this.l.QueryBasis(ql, qr, l, mid, res);
+				else if (ql >= mid) this.r.QueryBasis(ql, qr, mid, r, res);
 				else
 				{
 					this.l.QueryBasis(ql, mid, l, mid, res);
-					this.l.QueryBasis(mid, qr, mid, r, res);
+					this.r.QueryBasis(mid, qr, mid, r, res);
 				}
 			}
 		}
