ff5ffa5843c95dc2df66adcf40b8ea3600bc620c
Stalker <sqc1999@outlook.com>
2016-10-04 13:05:57 +0800

    Add Round #375 (Div. 2) E, F.

---
+++ Codeforces Round 375/One-Way Reform/main.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+const int N = 200;
+struct edge
+{
+	int to, id;
+	bool f;
+};
+vector<edge> g[N];
+int deg[N], gid[N];
+bool vis_v[N], vis_e[N*(N - 1) / 2 + N / 2];
+vector<pair<int, int>> ans;
+void dfs_fill(int u, int id)
+{
+	gid[u] = id;
+	for (const edge &e : g[u])
+		if (gid[e.to] == -1)
+			dfs_fill(e.to, id);
+}
+void dfs(int u, int x)
+{
+	vis_v[u] = true;
+	for (const edge &e : g[u])
+		if (!vis_e[e.id])
+		{
+			vis_e[e.id] = true;
+			deg[u]--;
+			deg[e.to]--;
+			if (!e.f) ans.push_back({ u + 1,e.to + 1 });
+			if (e.to != x) dfs(e.to, x);
+			break;
+		}
+}
+int solve(int id, int n, int m)
+{
+	memset(deg, 0x00, sizeof deg);
+	for (int i = 0; i < n; i++)
+		if (gid[i] == id)
+			for (const edge &e : g[i])
+			{
+				deg[i]++;
+				deg[e.to]++;
+			}
+	for (int i = 0; i < n; i++) deg[i] /= 2;
+	vector<int> odd;
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (gid[i] == id)
+			if (deg[i] % 2 == 1) odd.push_back(i);
+			else sum++;
+	}
+	for (int i = 0; i < odd.size(); i += 2)
+	{
+		g[odd[i]].push_back({ odd[i + 1],m + i / 2,true });
+		g[odd[i + 1]].push_back({ odd[i],m + i / 2,true });
+		deg[odd[i]]++;
+		deg[odd[i + 1]]++;
+	}
+	memset(vis_v, 0x00, sizeof vis_v);
+	memset(vis_e, 0x00, sizeof vis_e);
+	for (int i = 0; i < n; i++)
+		if (gid[i] == id)
+		{
+			dfs(i, i);
+			break;
+		}
+	while (true)
+	{
+		bool flag = false;
+		for (int i = 0; i < n; i++)
+			if (gid[i] == id && vis_v[i] && deg[i] > 0)
+			{
+				dfs(i, i);
+				flag = true;
+				break;
+			}
+		if (!flag) break;
+	}
+	return sum;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < n; i++) g[i].clear();
+		memset(deg, 0x00, sizeof deg);
+		for (int i = 0; i < m; i++)
+		{
+			int u, v;
+			cin >> u >> v;
+			u--, v--;
+			g[u].push_back({ v,i,false });
+			g[v].push_back({ u,i,false });
+		}
+		memset(gid, 0xff, sizeof gid);
+		int cnt = 0, sum = 0;
+		ans.clear();
+		for (int i = 0; i < n; i++)
+			if (gid[i] == -1)
+			{
+				dfs_fill(i, cnt);
+				sum += solve(cnt++, n, m);
+			}
+		cout << sum << endl;
+		for (const auto &p : ans) cout << p.first << ' ' << p.second << endl;
+	}
+}
---
+++ Codeforces Round 375/st-Spanning Tree/main.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+const int N = 2e5, M = 4e5;
+pair<int, int> e[M], es[N], et[N];
+int f[N];
+bool cs[N], ct[N];
+int find(int x) { return x == f[x] ? x : f[x] = find(f[x]); }
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> e[i].first >> e[i].second;
+		e[i].first--;
+		e[i].second--;
+	}
+	int s, t, ds, dt;
+	cin >> s >> t >> ds >> dt;
+	s--;
+	t--;
+	for (int i = 0; i < n; i++) f[i] = i;
+	vector<pair<int, int>> ans;
+	bool flag = false;
+	for (int i = 0; i < m; i++)
+	{
+		int u = e[i].first, v = e[i].second;
+		if (u == s&&v == t || u == t&&v == s) flag = true;
+		if (u != s&&u != t&&v != s&&v != t)
+		{
+			int x = find(u), y = find(v);
+			if (x != y)
+			{
+				f[x] = y;
+				ans.push_back({ u,v });
+			}
+		}
+	}
+	for (int i = 0; i < m; i++)
+	{
+		int u = e[i].first, v = e[i].second;
+		if (v == s) swap(u, v);
+		if (u == s&&v != t)
+		{
+			int x = find(v);
+			cs[x] = true;
+			es[x] = { u,v };
+		}
+		if (v == t) swap(u, v);
+		if (u == t&&v != s)
+		{
+			int x = find(v);
+			ct[x] = true;
+			et[x] = { u,v };
+		}
+	}
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (cs[i] && ct[i]) cnt++;
+		else if (cs[i])
+		{
+			if (--ds == 0) break;
+			ans.push_back(es[i]);
+		}
+		else if (ct[i])
+		{
+			if (--dt == 0) break;
+			ans.push_back(et[i]);
+		}
+	}
+	if (ds <= 0 || dt <= 0) cout << "No" << endl;
+	else if (ds + dt - 2 >= cnt&&flag)
+	{
+		ans.push_back({ s,t });
+		for (int i = 0; i < n; i++)
+			if (cs[i] && ct[i])
+			{
+				if (ds > 0)
+				{
+					ds--;
+					ans.push_back(es[i]);
+				}
+				else ans.push_back(et[i]);
+			}
+		cout << "Yes" << endl;
+		for (const auto &p : ans)
+			cout << p.first + 1 << ' ' << p.second + 1 << endl;
+	}
+	else if (ds + dt >= cnt + 1)
+	{
+		bool flag = true;
+		for (int i = 0; i < n; i++)
+			if (cs[i] && ct[i])
+			{
+				if (flag)
+				{
+					ds--;
+					flag = false;
+					ans.push_back(es[i]);
+					ans.push_back(et[i]);
+				}
+				else if (ds > 0)
+				{
+					ds--;
+					ans.push_back(es[i]);
+				}
+				else ans.push_back(et[i]);
+			}
+		cout << "Yes" << endl;
+		for (const auto &p : ans)
+			cout << p.first + 1 << ' ' << p.second + 1 << endl;
+	}
+	else cout << "No" << endl;
+}
